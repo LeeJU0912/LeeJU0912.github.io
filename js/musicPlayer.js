72d54b75acde19165f566379daccfd6994462efa
LeeJU_DESKTOP <thirdspirit@naver.com>
2023-03-16 03:51:15 +0900

    some fix

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -4,6 +4,7 @@ const stopBtn = document.querySelector('.js-stopBtn');
 const prevBtn = document.querySelector('.js-prevBtn');
 const nextBtn = document.querySelector('.js-nextBtn');
 const showMusicText = document.getElementById("musicText");
+const musicLink = document.getElementById("musicLink");
 
 const MUSIC_COUNT = 3;
 let currentAudio = 0;
@@ -13,6 +14,12 @@ const audioList = [
     "NEFFEX - Grateful",
     "Tobu & Itro - Sunburst",
 ];
+
+const audioLink = [
+    "https://youtu.be/CYDP_8UTAus",
+    "https://youtu.be/83RUhxsfLWs",
+    "https://youtu.be/1-0-4HqyvXE",
+]
 
 function playAudio() {
     //audioContainer.volume = 0.2;
@@ -24,6 +31,7 @@ function playAudio() {
     if (playAudio !== undefined) {
         playAudio.then((_) => {
             showMusicText.innerHTML = audioList[currentAudio] + " #NoCopyrightMusic";
+            musicLink.href = audioLink[currentAudio];
         }).catch((error) => {
         });
     }
@@ -31,6 +39,7 @@ function playAudio() {
 
 function stopAudio() {
     showMusicText.innerHTML = "";
+    musicLink.href = "https://youtu.be/c56t7upa8Bk"; // 이스터에그
     audioContainer.pause();
 }
 
